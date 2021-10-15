e9d9e0c34e9224c7dd4c03b6367293b822c62076
尾崎蒼太 <ozakisouta@ozakisoutanoMacBook-Air.local>
2021-10-15 12:45:52 +0900

    モデルの追加

--- database/migrations/2021_10_11_014103_create_posts_table.php
+++ database/migrations/2021_10_11_014103_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTestsTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,11 @@ class CreateTestsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tests', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignId('user_id')->constrained();
+            $table->string('name',255);
+            $table->text('contents');
         });
     }
 
@@ -26,6 +28,6 @@ class CreateTestsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tests');
+        Schema::dropIfExists('posts');
     }
 }
